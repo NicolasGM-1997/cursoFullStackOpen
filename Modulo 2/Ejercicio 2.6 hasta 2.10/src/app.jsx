e66d57ba8a76b3a7814de782749238dc7be3d460
Nicolas Gomez <nicolas.gomezm@uniagustiniana.edu.co>
2023-12-05 11:29:19 -0500

    Ejercicio 2.6

---
+++ Modulo 2/Ejercicio 2.6 hasta 2.10/src/app.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const enviarNombre = () =>{
+    event.preventDefault();
+    var copyPersons = [...persons]
+    copyPersons.push({name:newName})
+    setPersons(copyPersons)
+    setNewName('')
+  }
+
+  const cambioNombre = (event) =>{
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={enviarNombre}>
+        <div>
+          name: <input value={newName} onChange={cambioNombre}/>
+        </div>
+        <div>
+          <button>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person,i)=>(
+        <p key={i}>{person.name}</p>
+      ))}
+    </div>
+  )
+}
+
+
+root.render(
+  <App/>
+)
+
+export default App
