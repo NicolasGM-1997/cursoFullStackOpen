853d597d956554ceb4f15f7fcd6bd851a113fdcc
Nicolas Gomez <nicolas.gomezm@uniagustiniana.edu.co>
2023-12-05 12:04:07 -0500

    Ejercicio 2.8

--- Modulo 2/Ejercicio 2.6 hasta 2.10/src/app.jsx
+++ Modulo 2/Ejercicio 2.6 hasta 2.10/src/app.jsx
@@ -5,33 +5,40 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: '12345'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const enviarNombre = () =>{
+  const enviarDatos = () =>{
     event.preventDefault()
     const nombreExistente = persons.some((person) => person.name === newName)
     if(nombreExistente){
       alert(newName+"is already added to phonebook")
     } else {
       var copyPersons = [...persons]
-      copyPersons.push({name:newName})
+      copyPersons.push({name:newName, number:newNumber})
       setPersons(copyPersons)
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const cambioNombre = (event) =>{
     setNewName(event.target.value)
   }
 
+  const cambioNumero = (event) =>{
+     setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={enviarNombre}>
+      <form onSubmit={enviarDatos}>
         <div>
-          name: <input value={newName} onChange={cambioNombre}/>
+          <p>name: <input value={newName} onChange={cambioNombre}/></p>
+          <p>number: <input value={newNumber} onChange={cambioNumero}/></p>
         </div>
         <div>
           <button>add</button>
@@ -39,7 +46,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person,i)=>(
-        <p key={i}>{person.name}</p>
+        <p key={i}>{person.name} : {person.number}</p>
       ))}
     </div>
   )
