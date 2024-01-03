70d90bc91c63049657f84d6878792f64f5aa064d
Nicolas Gomez <nicolas.gomezm@uniagustiniana.edu.co>
2024-01-03 15:57:27 -0500

    Ejercicios 4.3 y 4.7

---
+++ Modulo 4/Ejercicios 4.1 hasta 4.7/utils/for_testing.js
@@ -0,0 +1,24 @@
+const palindrome = (string) => {
+  return string
+    .split('')
+    .reverse()
+    .join('')
+}
+
+const average = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  return array.reduce(reducer, 0) / array.length
+}
+
+const dummy = (blogs) => {
+  return 1
+}
+
+module.exports = {
+  palindrome,
+  average,
+  dummy,
+}
